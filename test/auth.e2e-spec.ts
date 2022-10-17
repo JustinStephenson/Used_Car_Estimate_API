fc8bf2eb449d21f1d79308bfc230e07b5334b9af
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2022-10-16 21:44:59 -0400

    adding e2e tests, and configs

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -29,4 +29,23 @@ describe('Authentication System (e2e)', () => {
         expect(email).toBe(testEmail);
       });
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const testEmail = 'e2e2@test.com';
+    const testPassword = 'e2e123';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: testEmail, password: testPassword })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(testEmail);
+  });
 });
---
+++ test/setup.ts
@@ -0,0 +1,10 @@
+import { rm } from 'fs/promises';
+import { join } from 'path';
+
+global.beforeEach(async () => {
+  try {
+    await rm(join(__dirname, '..', 'test.sqlite'));
+  } catch (err) {
+    // do nothing, if files does not exist it is okay.
+  }
+});
