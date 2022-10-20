0d25d2bd81aa4eae5f7f7bef9cd88da65b93bda2
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2022-10-19 20:11:59 -0400

    adding association from reports to a user

---
+++ src/reports/dtos/report.dto.ts
@@ -0,0 +1,33 @@
+import { Expose, Transform } from 'class-transformer';
+//import { User } from '../../users/user.entity';
+
+export class ReportDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  price: number;
+
+  @Expose()
+  year: number;
+
+  @Expose()
+  lng: number;
+
+  @Expose()
+  lat: number;
+
+  @Expose()
+  make: string;
+
+  @Expose()
+  model: string;
+
+  @Expose()
+  mileage: number;
+
+  // take original report entity and pull out user id, obj
+  @Transform(({ obj }) => obj.user.id)
+  @Expose()
+  userId: number;
+}
